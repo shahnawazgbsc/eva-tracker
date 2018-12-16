93b36dcc59799bca0b8f6c795bd08611daac20e3
Anus Kaleem <anus.kaleem@gbscsolutions.com>
2018-12-16 15:47:47 +0500

    Create MarketListScreen.js
    Added MarketListScreen

---
+++ App/Containers/Market/MarketListScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View} from 'react-native'
+import {Colors} from '../../Themes'
+import SectionExample from '../../../ignite/Examples/Containers/ignite-ir-boilerplate-andross/SectionExample'
+
+export default class MarketListScreen extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <SectionExample/>
+        )
+    }
+}
