8a0ceca57278048bdabe2db49ed2865d5d35d4df
anus.kaleem <anuskaleem@gmail.com>
2019-02-03 21:02:33 +0500

    Update Firebase.js
    Removed timestamps from settings object in an attempt of fixing a random crash

--- App/Lib/Firebase.js
+++ App/Lib/Firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase'
 import 'firebase/firestore'
 import AppConfig from '../Config/AppConfig'
 
-const settings = { timestampsInSnapshots: true }
+const settings = {  }
 
 if (!firebase.apps.length) {
   firebase.initializeApp(AppConfig.firebaseConfig)
