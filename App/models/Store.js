67d8e964bd83f0f175cc9c8e819c816cefa90ff9
anus.kaleem <anuskaleem@gmail.com>
2019-02-25 00:36:36 +0500

    Update Store.js
    Updated Store Model

--- App/models/Store.js
+++ App/models/Store.js
@@ -1,11 +1,14 @@
-import moment from 'moment';
+import Moment from 'moment';
 export default Store = { 
     name: 'Store',
     primaryKey: 'id',
     properties: { 
-        id: {type:'int'}, 
-        image: { type: 'data'},
-        StartTime: { type: 'date', optional: true, default: moment().format("MM/DD/YYYY HH:mm") },
+        id: {type:'string'}, 
+        imageHeight: { type: 'int'},
+        imageTimestamp:{type:'string'},
+        imageUri:{type:'string'},
+        imageData:{type:'string'},
+        StartTime: { type: 'date', optional: true, default: Moment().format("MM/DD/YYYY HH:mm") },
         shopName:  { type: 'string' },
         shopKeeper: { type: 'string'},
         contactNo: { type: 'string'},
@@ -16,10 +19,10 @@ export default Store = {
         landMark: { type: 'string'},
         cnic: { type: 'string', optional: true, default: '' },
         activeStatus: { type: 'bool', default: true },
-        category: { type: 'int'},
+        category: { type: 'string'},
         subsectionId:{ type: 'int' },
-        classification: { type: 'int'},
-        days:{ type: 'string[]' },
+        classification: { type: 'string'},
+        days:{ type: 'int[]' },
         status:{type:'int',default:0}
     },
 };
