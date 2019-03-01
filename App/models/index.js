c277ea0b5239f28b66d312e5d6ac06b4f8f5cb28
anus.kaleem <anuskaleem@gmail.com>
2019-03-01 10:38:39 +0500

    Update index.js
    included General SKU model

--- App/models/index.js
+++ App/models/index.js
@@ -4,7 +4,8 @@ import Inventory from './Inventory'
 import StoreVisits from './StoreVisits'
 import InventoryItems from './InventoryItems'
 import Brands from './Brands'
+import GeneralSkus from './GeneralSkus'
 
-export const Schema = [InventoryItems,Brands,Store,Order,Inventory,StoreVisits];
+export const Schema = [InventoryItems,Brands,Store,Order,Inventory,StoreVisits,GeneralSkus];
 
 export const SchemaVersion = 1;
