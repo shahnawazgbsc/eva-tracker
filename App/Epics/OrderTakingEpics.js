ccf0694818287623b4f0d611aadcfc18f5b35f3b
anus.kaleem <anuskaleem@gmail.com>
2019-03-11 00:59:34 +0500

    Update OrderTakingEpics.js
    updated api calls for offline mode.

--- App/Epics/OrderTakingEpics.js
+++ App/Epics/OrderTakingEpics.js
@@ -6,30 +6,36 @@ import * as R from 'ramda'
 import moment from 'moment'
 import ShopActions, { ShopTypes } from '../Redux/ShopRedux'
 import 'firebase/firestore'
+import realm from '../Database/realm'
 
 export const checkInEpic = (action$, state$, { api }) => action$.pipe(
   ofType(ShopTypes.CHECK_IN_REQUEST),
   mergeMap(action => {
-    const companyId = state$.value.login.payload.user.companyid
-    const latitude = state$.value.gps.data && state$.value.gps.data.latitude
-    const longitude = state$.value.gps.data && state$.value.gps.data.longitude
-
-    if (latitude && longitude) {
+    // const companyId = state$.value.login.payload.user.companyid
+    // const latitude = state$.value.gps.data && state$.value.gps.data.latitude
+    // const longitude = state$.value.gps.data && state$.value.gps.data.longitude
+    if (action.data.latitude && action.data.longitude) {
       const data = {
         StoreId: action.data.storeId,
         pjp: !!action.data.pjp,
-        companyId,
-        latitude,
-        longitude,
+        companyId:action.data.companyId,
+        latitude:action.data.latitude,
+        longitude:action.data.latitude,
         ContactPersonName: '',
         ContactNo: '',
-        StartTime: moment().format("MM/DD/YYYY HH:mm"),
+        StartTime: action.data.StartTime,
         Status: 'Pending',
         NextScheduledVisit: ''
       }
       return api.checkIn(data).pipe(
         map(response => {
           if (response.ok) {
+            realm.write(()=>{
+              realm.create('CheckIn',{
+                StoreId:action.data.storeId,
+                StoreVisitId:response.data.storeVisitId
+              },true)
+            })
             return ShopActions.checkInSuccess({ ...data, storeVisitId: response.data.storeVisitId })
           } else {
             return ShopActions.shopFailure(response)
@@ -46,27 +52,22 @@ export const checkInEpic = (action$, state$, { api }) => action$.pipe(
 export const checkOutEpic = (action$, state$, { api, firebase }) => action$.pipe(
   ofType(ShopTypes.CHECK_OUT_REQUEST),
   mergeMap(action => {
-    const latitude = state$.value.gps.data && state$.value.gps.data.latitude
-    const longitude = state$.value.gps.data && state$.value.gps.data.longitude
-
-    const checkInParam = state$.value.shop.checkInParam
-    const userId = state$.value.login.payload.user.userid
-
-    if (latitude && longitude) {
+alert(JSON.stringify(action))
+    if (action.data.shop.latitude && action.data.shop.longitude) {
       const data = {
-        StoreId: checkInParam.StoreId,
-        pjp: !!action.data.pjp,
-        companyId: checkInParam.companyId,
-        latitude,
-        longitude,
+        StoreId: action.data.shop.StoreId,
+        pjp: !!action.data.shop.pjp,
+        companyId: action.data.shop.companyId,
+        latitude:action.data.shop.latitude,
+        longitude:action.data.shop.longitude,
         ContactPersonName: '',
         ContactNo: '',
-        StartTime: checkInParam.StartTime,
+        StartTime: action.data.shop.StartTime,
         NextScheduledVisit: '',
         Location: '',
         Notes: '',
-        EndTime: moment().format("MM/DD/YYYY HH:mm"),
-        StoreVisitId: checkInParam.storeVisitId,
+        EndTime: action.data.shop.EndTime,
+        StoreVisitId: action.data.shop.storeVisitId,
         Status: 'Achieved'
       }
       const dateNow = String(new Date().getMilliseconds())
@@ -77,50 +78,50 @@ export const checkOutEpic = (action$, state$, { api, firebase }) => action$.pipe
             firebase.firestore().batch().set(
               firebase.firestore()
                 .collection('tbl_shops')
-                .doc(String(checkInParam.StoreId))
+                .doc(String(action.data.shop.StoreId))
                 .collection('shop_events')
                 .doc(dateNow),
               {
                 device_name: 'ABC_Device',
-                lng: latitude,
-                lat: longitude,
-                shopId: String(checkInParam.StoreId),
+                lng: action.data.shop.latitude,
+                lat: action.data.shop.longitude,
+                shopId: String(action.data.shop.StoreId),
                 eventId: dateNow,
-                userId: String(userId),
+                userId: String(action.data.userId),
                 timestamp: moment().format("MM/DD/YYYY HH:mm")
               }).set(firebase.firestore().collection('tbl_shops')
-                .doc(String(checkInParam.StoreId))
+                .doc(String(action.data.shop.StoreId))
                 .collection('visit_summary')
                 .doc(dateNow),
               {
-                pjp: !!action.data.pjp,
-                productive: action.data.productive,
-                lat: latitude,
-                lng: longitude,
-                shopId: checkInParam.StoreId,
-                userId: userId,
+                pjp: !!action.data.shop.pjp,
+                productive: action.data.shop.productive,
+                lat: action.data.shop.latitude,
+                lng: action.data.shop.longitude,
+                shopId: action.data.shop.StoreId,
+                userId: action.data.userId,
                 visitId: Number.parseInt(dateNow),
                 timestamp: moment().format("MM/DD/YYYY HH:mm")
               }).commit()
 
             firebase.firestore().collection('tbl_shops')
-              .doc(String(checkInParam.StoreId))
+              .doc(String(action.data.shop.StoreId))
               .set(
                 {
-                  userId: userId,
-                  lng: longitude,
-                  lat: latitude,
-                  shopId: checkInParam.StoreId,
+                  userId: action.data.userId,
+                  lng: action.data.shop.longitude,
+                  lat: action.data.shop.latitude,
+                  shopId: action.data.shop.StoreId,
                   timestamp: moment().format("MM/DD/YYYY HH:mm")
                 })
 
             if (action.data.onSuccess) action.data.onSuccess()
             return ShopActions.checkOutSuccess(
               {
-                id: checkInParam.StoreId,
-                pjp: !!action.data.pjp,
-                productive: action.data.productive
-              }, userId
+                id: action.data.shop.StoreId,
+                pjp: !!action.data.shop.pjp,
+                productive: action.data.shop.productive
+              }, action.data.userId
             )
           } else {
             return ShopActions.shopFailure(response)
@@ -150,14 +151,12 @@ export const nonProductiveReasonsEpics = (action$, state$, { api }) => action$.p
 export const placeOrderEpics = (action$, state$, { api }) => action$.pipe(
   ofType(ShopTypes.PLACE_ORDER_REQUEST),
   mergeMap(action => {
-    const checkInParams = state$.value.shop.checkInParam
-    const userId = state$.value.login.payload.user.userid
     const Order = action.data.items.map(value => ({
       ...value,
-      storeVisitId: checkInParams.storeVisitId,
-      companyId: checkInParams.companyId,
-      StoreId: checkInParams.StoreId,
-      userId: userId
+      storeVisitId: action.data.checkIn.storeVisitId,
+      companyId: action.data.checkIn.companyId,
+      StoreId: action.data.checkIn.StoreId,
+      userId: action.data.userId
     }))
 
     const hasReason = R.has('noorderreason')(action.data.items[0])
@@ -177,7 +176,7 @@ export const placeOrderEpics = (action$, state$, { api }) => action$.pipe(
             companyId: value.companyId,
             storeId: value.storeId,
             extraDiscount: extra,
-            userId: userId
+            userId: action.data.userId
           }))
           return api.salesIndents(array)
         }
