b0ff5c78a7c030f0362dc040ab97accef0bf5693
anus.kaleem <anuskaleem@gmail.com>
2019-03-11 01:00:32 +0500

    Create NonProductiveReasons.js
    Included new model for non productive reasons.

---
+++ App/models/NonProductiveReasons.js
@@ -0,0 +1,11 @@
+export default NonProductiveReasons = { 
+    name: 'NonProductiveReasons',
+    primaryKey:'id',
+    properties: { 
+        id: {type:'string'},
+        reason:{type:'string'},
+        companyId:{type:'string'},
+        reason:{type:'string'}
+    },
+};
+
