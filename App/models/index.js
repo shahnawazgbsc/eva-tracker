57c74603245be5557bed88abb395ac59795172be
anus.kaleem <anuskaleem@gmail.com>
2019-03-11 01:00:06 +0500

    Update index.js
    Updated schema

--- App/models/index.js
+++ App/models/index.js
@@ -5,7 +5,10 @@ import StoreVisits from './StoreVisits'
 import InventoryItems from './InventoryItems'
 import Brands from './Brands'
 import GeneralSkus from './GeneralSkus'
+import CheckIn from './CheckIn'
+import NonProductiveReasons from './NonProductiveReasons'
+import NoOrderReason from './NoOrderReason'
 
-export const Schema = [InventoryItems,Brands,Store,Order,Inventory,StoreVisits,GeneralSkus];
+export const Schema = [NoOrderReason,NonProductiveReasons,InventoryItems,Brands,Store,Order,Inventory,StoreVisits,GeneralSkus,CheckIn];
 
 export const SchemaVersion = 1;
